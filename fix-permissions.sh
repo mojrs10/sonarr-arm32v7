93829c22016d025f2e57aaadfbc4885076f7fc53
mojrs10 <64679555+mojrs10@users.noreply.github.com>
2025-03-13 09:42:55 +0100

    Update fix-permissions.sh

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -8,15 +8,16 @@ git config --global user.email "actions@github.com"
 
 echo "🔧 Postavljam prava na izvršavanje za potrebne fajlove..."
 
+# Promijenjeno iz 'radarr' u 'sonarr'
 chmod 755 root/donate.txt
-chmod 755 root/etc/s6-overlay/s6-rc.d/init-radarr-config/run
-chmod 755 root/etc/s6-overlay/s6-rc.d/svc-radarr/run
-chmod 755 root/etc/s6-overlay/s6-rc.d/svc-radarr/data/check
+chmod 755 root/etc/s6-overlay/s6-rc.d/init-sonarr-config/run
+chmod 755 root/etc/s6-overlay/s6-rc.d/svc-sonarr/run
+chmod 755 root/etc/s6-overlay/s6-rc.d/svc-sonarr/data/check
 
 git add root/donate.txt
-git add root/etc/s6-overlay/s6-rc.d/init-radarr-config/run
-git add root/etc/s6-overlay/s6-rc.d/svc-radarr/run
-git add root/etc/s6-overlay/s6-rc.d/svc-radarr/data/check
+git add root/etc/s6-overlay/s6-rc.d/init-sonarr-config/run
+git add root/etc/s6-overlay/s6-rc.d/svc-sonarr/run
+git add root/etc/s6-overlay/s6-rc.d/svc-sonarr/data/check
 
 # Provjeri je li bilo promjena
 if ! git diff-index --quiet HEAD --; then
